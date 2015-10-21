a73d7497ee224f4741ef29e312843459c3f6c253
László Dobó <dobo.laszlou@gmail.com>
2015-10-21 22:34:19 +0200

    created front page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var models = require('../models');
+var moment = require('moment');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  models.Measure.findAll({ limit: 10, order: [['date', 'DESC']]}).then(function (measures) {
+    measures.forEach(function (measure) {
+      console.log(measure.get());
+    });
+  });
+
+  models.Measure.findOne({ order: [['date', 'DESC']]}).then(function (measure) {
+    console.log(measure.get())
+    res.render('index', { title: 'Temperate monitor' , measure: measure.get(), moment: moment});
+  });
 });
 
 module.exports = router;
